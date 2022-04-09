5781727397162d84a4505584638d5a1016e33808
BlackBo6 <achrafboucetta00@gmail.com>
2022-04-09 05:44:35 +0000

    integration de mongodb avec laravel+configuration

--- backend/config/database.php
+++ backend/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -34,6 +34,11 @@ return [
     */
 
     'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => 'mongodb+srv://master_ged:8DEZkqJa7LB7qmN4@cluster0.fuaqw.mongodb.net/ged?retryWrites=true&w=majority',
+            'database' => 'ged',
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -125,7 +130,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
