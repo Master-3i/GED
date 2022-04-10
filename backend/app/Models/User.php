ff4897042cb24373b8e7bd302c150567624c7cf4
BlackBo6 <achrafboucetta00@gmail.com>
2022-04-10 09:53:39 +0000

    configure auth models with mongodb

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -18,8 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
+        'phone_num',
+        'email_verified',
         'password',
     ];
 
@@ -41,4 +44,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $connection = 'mongodb';
 }
